240b0436a4bcbf356ae21650d6e01885c497e957
miguelfbrito000 <miguelferreiradebrito681@gmail.com>
2025-06-13 20:00:12 -0300

    feat (pages home) alterei a cor da pagina home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,18 @@
 export const HomePage = () => {
     return (
-        <div className="w-full flex flex-col items-center justify-center">
-            <h1>Home</h1>
+        <div className="w-full flex flex-col items-center p-8 h-screen bg-gradient-to-t from-[#8F824C] to-[#F5DE82]">
+            <h1>Ecos do passado</h1>    
+
+            <div></div>
+            <section>
+    
+            </section>
+            <article></article>
+
+           <h2>Seja bem vindo ao nosso aplicativo</h2>
+            <h5></h5>
+            <p> </p>
+            <span></span>
         </div>
     )
 }
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,7 @@
+export const LoginPage = () => {
+    return (
+        <div className="w-full flex flex-col items-center justify-center">
+            <h1>Login</h1>
+        </div> 
+    )
+}
