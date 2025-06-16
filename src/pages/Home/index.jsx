94abd5d12f868d94cb29ee17bf533867e95af10b
miguelfbrito000 <miguelferreiradebrito681@gmail.com>
2025-06-16 20:04:13 -0300

    feat (pages home): alterei a cor e criei um botão entrar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,12 @@
 export const HomePage = () => {
     return (
-<<<<<<< HEAD
-        <div className="w-full flex flex-col items-center p-8 h-screen bg-gradient-to-t from-[#8F824C] to-[#F5DE82]">
-            <h1>Ecos do passado</h1>    
+        <div className="w-full flex flex-col items-center justify-between p-8 h-screen bg-gradient-to-t from-[#8F824C] to-[#F5DE82]">
 
-            <div></div>
-            <section>
-    
-            </section>
-            <article></article>
+            <h1>Ecos do passado.</h1>
+            <p>Aqui, você irá ver as melhores histórias e lendas da cidade Araraquara!</p>
+            <button>entrar</button>
 
-           <h2>Seja bem vindo ao nosso aplicativo</h2>
-            <h5></h5>
-            <p> </p>
-            <span></span>
-=======
-        <div className="w-full flex flex-col items-center justify-center">
-            
-            <h1>Home</h1>
-            <div>
-                
-            </div>
->>>>>>> 691739144f38aa5fbadaeff273e6e77bae072cf6
+
         </div>
     )
 }
