cae36ceb1cda4b9f40db3b75fe861a00052198c8
miguelfbrito000 <miguelferreiradebrito681@gmail.com>
2025-06-17 11:44:29 +0000

    refactor (pages): editei as paginas login e home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,18 @@
 export const HomePage = () => {
     return (
-<<<<<<< HEAD
-        <div className="w-full flex flex-col items-center p-8 h-screen bg-gradient-to-t from-[#8F824C] to-[#F5DE82]">
-            <h1>Ecos do passado</h1>    
-
+        <div className="w-full flex flex-col items-center p-8 h-dvh">    
+            <img src='/Logo.png' width={200} height={200} />
             <div></div>
             <section>
     
             </section>
             <article></article>
 
-           <h2>Seja bem vindo ao nosso aplicativo</h2>
+           <h2 className="text-[#3F3A33] drop-shadow-lg drop-shadow-[#3F3A33]">Aqui, você irá ver as melhores histórias e lendas da cidade Araraquara!</h2>
             <h5></h5>
             <p> </p>
             <span></span>
-=======
-        <div className="w-full flex flex-col items-center justify-center">
-            
-            <h1>Home</h1>
-            <div>
-                
-            </div>
->>>>>>> 691739144f38aa5fbadaeff273e6e77bae072cf6
+            <button className="mb-10 absolute bottom-0 bg-gray-300/50 text-gray-950 p-4 border-gray-900 rounded-full max-w-2xl min-w-md w-1/2 text-lg font-bold italic">Entrar</button>
         </div>
     )
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,36 +1,23 @@
 export const LoginPage = () => {
     return (
-<<<<<<< HEAD
-        <div className="w-full flex flex-col items-center justify-center">
-            <h1>Login</h1>
-        </div> 
-    )
-}
-=======
-        <div className="w-full flex flex-col items-center justify-between p-8 h-screen bg-gradient-to-t from-[#8F824C] to-[#F5DE82]">
-            <h1>Ecos do Passado</h1>
-
-            <img src='\logo192.png' width={200} height={200} />
-            <div className="w-full max-w-lg flex items-center justify-center">
-
+        <div className="w-full flex flex-col items-center justify-between p-8 h-dvh">
+            <img src='/Logo.png' width={200} height={200} />
+            <h2>Seja bem vindo ao nosso site!</h2>
+            <div className="w-full max-w-lg flex flex-col items-center justify-center">
                 <form className="w-full flex max-w-xs flex-col gap-4">
                     <fieldset className="flex flex-col w-full gap-2">
-                        <label htmlFor="title">Login</label>
-                        <input type="text" id="title"  className="border p-2 rounded-lg" />
+                        <input placeholder="Login" type="text" id="title" className="border p-2 rounded-lg" />
                     </fieldset>
 
-         <form className="w-full flex max-w-xs flex-col gap-4"> </form>
-                    <fieldset >
-                        <label htmlFor="description">Senha</label>
-                        <input type="text" id="description" />
+                    <fieldset className="flex flex-col w-full gap-2">
+                        <input placeholder="Senha" type="password" id="description" className="border p-2 rounded-lg" />
                     </fieldset>
 
-                    <button type="submit">Entrar</button>
+                    <button className="mb-10 bottom-0 bg-gray-300/50 text-gray-950 p-4 border-gray-900 rounded-full max-w-2xl min-w-md w-full text-lg font-bold italic" type="submit">Entrar</button>
 
                 </form>
             </div>
             <section className="">
-                <h2>Seja bem vindo ao nosso site!</h2>
                 <ul>
                 </ul>
             </section>
@@ -38,4 +25,3 @@ export const LoginPage = () => {
 
     )
 }
->>>>>>> 691739144f38aa5fbadaeff273e6e77bae072cf6
